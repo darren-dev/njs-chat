f9c86b81dbe307fcaaa4d87af9d31eb12c2a0d86
Darren Whitfield <darren.whitfield@gmail.com>
2014-08-28 08:06:06 +0200

    I should really have some better commit messages

---
+++ NJS-Chat/Helpers/UserHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NJS_Chat.Helpers
+{
+    public class UserHelper
+    {
+        public void RegisterUser(string username, string password)
+        {
+            
+        }
+    }
+}
